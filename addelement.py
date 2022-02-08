5f12ed8ee751d597ea2d0c267b23dfe6f4269b7c
Hager27 <99009596+Hager27@users.noreply.github.com>
2022-02-08 00:26:40 -0800

    Create addelement.py
    Adding new elements to existing array list

---
+++ addelement.py
@@ -0,0 +1,14 @@
+brands = ["Coke","Apple","Nokia","Google","Toyota"]
+print(brands)
+#length of array
+len_brands = len(brands)
+print(len_brands)
+#Adding an element to an array using append()
+brands.append("Intel")
+print(brands)
+print(brands[0])
+print(brands[1])
+print(brands[2])
+print(brands[3])
+print(brands[4])
+print(brands[5])
