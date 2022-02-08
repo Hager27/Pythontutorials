155af0cccfd79782bd63658d621715ab2abcebc8
Hager27 <99009596+Hager27@users.noreply.github.com>
2022-02-08 00:31:03 -0800

    Create modfyelt.py
    Modifying single array element

---
+++ modfyelt.py
@@ -0,0 +1,7 @@
+#modfying aray element using indexing
+fruits =["Orange","Mango","Apple","Banana","Tringo","Lomi"]
+print(fruits)
+fruits[1] = "Papaya"
+print(fruits)
+fruits[-1] = "Pineapple"
+print(fruits)
