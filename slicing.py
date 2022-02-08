d917ee3ccff2de4ed1988e6d3583c66a6841e6d0
Hager27 <99009596+Hager27@users.noreply.github.com>
2022-02-08 00:35:40 -0800

    Create slicing.py
    slicing an array

---
+++ slicing.py
@@ -0,0 +1,8 @@
+#slicing an array
+fruits = ["tella","tej","Weyin","Filiter","Korefe","Birz","Chimaki"]
+print(fruits)
+print(fruits[1:4])
+print(fruits[:3])
+print(fruits[-4:])
+print(fruits[-3:-1])
+print(fruits)
