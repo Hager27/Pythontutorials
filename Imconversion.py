5df55b093e38fbe68355d57b9b6af2ef1e747163
Hager27 <99009596+Hager27@users.noreply.github.com>
2022-02-08 00:14:07 -0800

    Create Imconversion.py
    Python tutorial for implicit type conversion

---
+++ Imconversion.py
@@ -0,0 +1,9 @@
+#https://github.com/Hager27/Pythontutorials.git
+#Implicit type conversion
+num_int = 123
+num_float =1.23
+num_new = num_int + num_float
+print("datatype of num_int:", type(num_int))
+print("datatype of num_float:", type(num_float))
+print("value of num_new:", num_new)
+print("datatype of num_new:", type(num_new))
